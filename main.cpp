59edb7e68b6c3d7fcff446b32ca7e52c3714cdc5
LEONARDO NISHIHARA <lnishihara@fi.uba.ar>
2026-05-29 22:40:55 +0000

    repository updated

--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
 //=====[Libraries]=============================================================
 
-#include "smart_home_system.h"
+#include "mbed.h"
+#include "user_interface.h"
 
 //=====[Main function, the program entry point after power on or reset]========
 
 int main()
 {
-    smartHomeSystemInit();
+    // Inicializa la pantalla
+    userInterfaceInit();
+
     while (true) {
-        smartHomeSystemUpdate();
+        // Actualiza y refresca los datos del display constantemente
+        userInterfaceUpdate();
+
+        // Pequeña pausa de 10 milisegundos para no saturar el procesador
+        // y sincronizar con el tiempo de refresco que configuramos antes
+        thread_sleep_for(10);
     }
 }
--- modules/user_interface/user_interface.cpp
+++ modules/user_interface/user_interface.cpp
@@ -4,47 +4,26 @@
 #include "arm_book_lib.h"
 
 #include "user_interface.h"
-
-#include "code.h"
-#include "siren.h"
-#include "smart_home_system.h"
-#include "fire_alarm.h"
-#include "date_and_time.h"
-#include "temperature_sensor.h"
-#include "gas_sensor.h"
-#include "matrix_keypad.h"
 #include "display.h"
 
 //=====[Declaration of private defines]========================================
 
 #define DISPLAY_REFRESH_TIME_MS 1000
 
+// Define the time step directly here since the keypad module is gone
+#define SYSTEM_TIME_INCREMENT_MS 10 
+
 //=====[Declaration of private data types]=====================================
 
 //=====[Declaration and initialization of public global objects]===============
-
-DigitalOut incorrectCodeLed(LED3);
-DigitalOut systemBlockedLed(LED2);
 
 //=====[Declaration of external public global variables]=======================
 
 //=====[Declaration and initialization of public global variables]=============
 
-char codeSequenceFromUserInterface[CODE_NUMBER_OF_KEYS];
-
 //=====[Declaration and initialization of private global variables]============
 
-static bool incorrectCodeState = OFF;
-static bool systemBlockedState = OFF;
-
-static bool codeComplete = false;
-static int numberOfCodeChars = 0;
-
 //=====[Declarations (prototypes) of private functions]========================
-
-static void userInterfaceMatrixKeypadUpdate();
-static void incorrectCodeIndicatorUpdate();
-static void systemBlockedIndicatorUpdate();
 
 static void userInterfaceDisplayInit();
 static void userInterfaceDisplayUpdate();
@@ -53,81 +32,15 @@ static void userInterfaceDisplayUpdate();
 
 void userInterfaceInit()
 {
-    incorrectCodeLed = OFF;
-    systemBlockedLed = OFF;
-    matrixKeypadInit( SYSTEM_TIME_INCREMENT_MS );
     userInterfaceDisplayInit();
 }
 
 void userInterfaceUpdate()
 {
-    userInterfaceMatrixKeypadUpdate();
-    incorrectCodeIndicatorUpdate();
-    systemBlockedIndicatorUpdate();
     userInterfaceDisplayUpdate();
 }
 
-bool incorrectCodeStateRead()
-{
-    return incorrectCodeState;
-}
-
-void incorrectCodeStateWrite( bool state )
-{
-    incorrectCodeState = state;
-}
-
-bool systemBlockedStateRead()
-{
-    return systemBlockedState;
-}
-
-void systemBlockedStateWrite( bool state )
-{
-    systemBlockedState = state;
-}
-
-bool userInterfaceCodeCompleteRead()
-{
-    return codeComplete;
-}
-
-void userInterfaceCodeCompleteWrite( bool state )
-{
-    codeComplete = state;
-}
-
 //=====[Implementations of private functions]==================================
-
-static void userInterfaceMatrixKeypadUpdate()
-{
-    static int numberOfHashKeyReleased = 0;
-    char keyReleased = matrixKeypadUpdate();
-
-    if( keyReleased != '\0' ) {
-
-        if( sirenStateRead() && !systemBlockedStateRead() ) {
-            if( !incorrectCodeStateRead() ) {
-                codeSequenceFromUserInterface[numberOfCodeChars] = keyReleased;
-                numberOfCodeChars++;
-                if ( numberOfCodeChars >= CODE_NUMBER_OF_KEYS ) {
-                    codeComplete = true;
-                    numberOfCodeChars = 0;
-                }
-            } else {
-                if( keyReleased == '#' ) {
-                    numberOfHashKeyReleased++;
-                    if( numberOfHashKeyReleased >= 2 ) {
-                        numberOfHashKeyReleased = 0;
-                        numberOfCodeChars = 0;
-                        codeComplete = false;
-                        incorrectCodeState = OFF;
-                    }
-                }
-            }
-        }
-    }
-}
 
 static void userInterfaceDisplayInit()
 {
@@ -148,12 +61,12 @@ static void userInterfaceDisplayUpdate()
     static int accumulatedDisplayTime = 0;
     char temperatureString[3] = "";
     
-    if( accumulatedDisplayTime >=
-        DISPLAY_REFRESH_TIME_MS ) {
+    if( accumulatedDisplayTime >= DISPLAY_REFRESH_TIME_MS ) {
 
         accumulatedDisplayTime = 0;
 
-        sprintf(temperatureString, "%.0f", temperatureSensorReadCelsius());
+        // Static display values to maintain layout without external sensors
+        sprintf(temperatureString, "%.0f", 25.0); 
         displayCharPositionWrite ( 12,0 );
         displayStringWrite( temperatureString );
         displayCharPositionWrite ( 14,0 );
@@ -161,7 +74,8 @@ static void userInterfaceDisplayUpdate()
 
         displayCharPositionWrite ( 4,1 );
 
-        if ( gasDetectorStateRead() ) {
+        bool gasDetected = false; 
+        if ( gasDetected ) {
             displayStringWrite( "Detected    " );
         } else {
             displayStringWrite( "Not Detected" );
@@ -169,24 +83,14 @@ static void userInterfaceDisplayUpdate()
 
         displayCharPositionWrite ( 6,2 );
         
-        if ( sirenStateRead() ) {
+        bool alarmOn = false;
+        if ( alarmOn ) {
             displayStringWrite( "ON " );
         } else {
             displayStringWrite( "OFF" );
         }
 
     } else {
-        accumulatedDisplayTime =
-            accumulatedDisplayTime + SYSTEM_TIME_INCREMENT_MS;        
+        accumulatedDisplayTime = accumulatedDisplayTime + SYSTEM_TIME_INCREMENT_MS;        
     } 
-}
-
-static void incorrectCodeIndicatorUpdate()
-{
-    incorrectCodeLed = incorrectCodeStateRead();
-}
-
-static void systemBlockedIndicatorUpdate()
-{
-    systemBlockedLed = systemBlockedState;
 }
